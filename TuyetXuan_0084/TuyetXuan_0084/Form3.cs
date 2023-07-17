eb7fe2c2cb4ef69b23990331338739582a87b316
DoThiTuyetXuan <139724489+DoThiTuyetXuan@users.noreply.github.com>
2023-07-17 10:18:30 +0700

    bt

---
+++ TuyetXuan_0084/TuyetXuan_0084/Form3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baquangluan
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String txtUser = txtUsername.Text.ToString();
+            String txtPass = txtpassword.Text.ToString();
+            if (txtUser=="luan" && txtPass=="12345")
+            {
+                Form1 objForm01 = new Form1();
+                objForm01.ShowDialog();
+            }
+        }
+    }
+}
